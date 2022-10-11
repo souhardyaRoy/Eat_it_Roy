2dda61b93108747cc08f7fbe21a8c5a81c2b71e8
souhardyaRoy <109021836+souhardyaRoy@users.noreply.github.com>
2022-10-11 17:08:30 +0530

    2nd commit

--- src/Route/index.ts
+++ src/Route/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { user_registration_router } from "../User_Registration/user_resgistration.router";
 
 
 export const allRoutes: Record<string, Router> = {
-    
+    'user' : user_registration_router
 }
---
+++ src/User_Registration/user_resgistration.router.ts
@@ -0,0 +1,26 @@
+import express, { Router } from 'express';
+import { _userController } from './user_registration.controller';
+
+const user_registration_router: Router = express.Router();
+
+/*
+    ============== old method =====================
+    studentRouter.get('/all', getAllStudents)
+    studentRouter.get('/:student_id', getAllStudents)
+    studentRouter.post('/', ()=>{});
+    studentRouter.put('/', ()=>{});
+    studentRouter.delete('/', ()=>{});
+*/
+
+// new classic and sexy method.
+
+// ! there's still a major issue that exist in the API, please find that...
+// ! it's can be critical to server health.
+
+user_registration_router
+    .get('/all', (req, res) => _userController.find(res, {}))
+    .post('/', (req, res) => _userController.create(res, req.body))
+    .put('/:id', (req, res) => _userController.update(res, req.params.id, req.body))
+    .delete('/:id', (req, res) => _userController.delete(res, req.params.id))
+
+export { user_registration_router };
